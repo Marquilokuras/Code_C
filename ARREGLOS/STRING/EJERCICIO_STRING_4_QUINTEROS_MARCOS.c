ce93510707d4431eab86775fc4641b3dbff57221
Marcos Quinteros <marcos.quinteros2003@gmail.com>
2024-05-24 23:10:05 -0300

    Ejercicio 4 y 6 terminado

--- ARREGLOS/STRING/EJERCICIO_STRING_4_QUINTEROS_MARCOS.c
+++ ARREGLOS/STRING/EJERCICIO_STRING_4_QUINTEROS_MARCOS.c
@@ -1,42 +1,78 @@
 #include <stdio.h>
 #include <string.h>
 
-void carga(int alumnos[10],char nombres[10][10]){
-	int nota=0;
-	//char nombre[10];
-	for(int i=0;i<10;i++){
+void carga(int alumnos[10], char nombres[10][30]) {
+	int nota = 0;
+	for(int i = 0; i < 10; i++) {
 		printf("\nIngrese Nombre:\n");
-		fgets(nombres[i],10,stdin);
+		fgets(nombres[i], 30, stdin);
+		// Eliminar el salto de línea del nombre
+		nombres[i][strcspn(nombres[i], "\n")] = 0;
 		
-		//do{
-			printf("\nIngrese Promedio anual:\n");
-			scanf("%d",&nota);
-		//}while(nota<0 || nota>10);
-		alumnos[i]=nota;
+		do {
+			printf("Ingrese Promedio anual:\n");
+			scanf("%d", &nota);
+		} while(nota < 0 || nota > 10);
+		alumnos[i] = nota;
+		
+		// Limpiar el buffer del stdin
+		while (getchar() != '\n');
 	}
 }
+
+// Función para ordenar a los alumnos por méritos usando el método de burbuja
+void ordenMerito(int alumnos[10], char nombres[10][30]) {
+	int auxNota;
+	char auxNombre[30];
+	for(int i = 0; i < 10; i++) {
+		for(int j = 0; j < 10 - i - 1; j++) {
+			if(alumnos[j] < alumnos[j + 1]) {
+				// Intercambiar notas
+				auxNota = alumnos[j];
+				alumnos[j] = alumnos[j + 1];
+				alumnos[j + 1] = auxNota;
+				
+				// Intercambiar nombres
+				strcpy(auxNombre, nombres[j]);
+				strcpy(nombres[j], nombres[j + 1]);
+				strcpy(nombres[j + 1], auxNombre);
+			}
+		}
+	}
+}
+
+
+void imprimir(int alumnos[10], char nombres[10][30]) {
+	printf("\nAlumnos con notas entre 0 y 4:\n");
+	for(int i = 0; i < 10; i++) {
+		if(alumnos[i] >= 0 && alumnos[i] < 4) {
+			printf("%s - %d\n", nombres[i], alumnos[i]);
+		}
+	}
 	
-void ordenMerito(int alumnos[10],char nombres[10][10]){
-	int aux=0;
-	char nombre;
-	for(int i=0;i<10;i++){
-		if(alumnos[i]<alumnos[i+1]){
-			aux=alumnos[i+1];
-			//nombre=nombres[i+1];
-			alumnos[i+1]=alumnos[i];
-			//nombres[i+1]=nombres[i];
-			alumnos[i]=aux;
-			//nombres[i]=nombre;
+	printf("\nAlumnos con notas entre 4 y 7:\n");
+	for(int i = 0; i < 10; i++) {
+		if(alumnos[i] >= 4 && alumnos[i] < 7) {
+			printf("%s - %d\n", nombres[i], alumnos[i]);
+		}
+	}
+	
+	printf("\nAlumnos con notas entre 7 y 10:\n");
+	for(int i = 0; i < 10; i++) {
+		if(alumnos[i] >= 7 && alumnos[i] <= 10) {
+			printf("%s - %d\n", nombres[i], alumnos[i]);
 		}
 	}
 }
 
 int main() {
-	int alumnos[10]={0};
-	char nombres[10][10];
+	int alumnos[10] = {0};
+	char nombres[10][30];
 	
-	carga(alumnos,nombres);
-	ordenMerito(alumnos,nombres);
-	//imprimir(alumnos);
+	carga(alumnos, nombres);
+	ordenMerito(alumnos, nombres);
+	imprimir(alumnos, nombres);
+	
 	return 0;
 }
+
---
+++ ARREGLOS/STRING/EJERCICIO_STRING_6_QUINTEROS_MARCOS.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+int esCodigoCorrecto(int codigo[4]) {
+	int A = codigo[0];
+	int B = codigo[1];
+	int C = codigo[2];
+	int D = codigo[3];
+	
+	
+	if (A == B || A == C || A == D || B == C || B == D || C == D) {
+		return 0;
+	}
+	
+	if (A + B + C + D != 23) {
+		return 0;
+	}
+	
+	if ((A * B) - D + C != 8) {
+		return 0;
+	}
+	
+	if (D - A - B - C != -5) {
+		return 0;
+	}
+	
+	if (A - B + C + D != 13) {
+		return 0;
+	}
+	
+	return 1;
+}
+
+int main() {
+	int codigo[4] = {0},intentos=0,acierto=0;
+	
+	do{
+		
+		printf("\nIngrese codigo de 4 digitos:\n");
+		
+		for (int i = 0; i < 4; i++) {
+			printf("Ingrese numero de codigo %d: ", i + 1);
+			scanf("%d", &codigo[i]);
+		}
+		
+		if (esCodigoCorrecto(codigo)) {
+			acierto = 1;
+			printf("\nCODIGO CORRECTO\n");
+			break;
+		} else {
+			printf("\nCodigo incorrecto.\n");
+			intentos++;
+		}
+		
+	} while(intentos!=3);
+	
+	return 0;
+}
+
